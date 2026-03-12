5b207cac4b9b9608cdc363f30710f3e2c698cad4
VaryaPolenina <167534895+VaryaPolenina@users.noreply.github.com>
2026-03-12 18:37:43 +0300

    №5 средний тип, ищем R

---
+++ №5 средний тип, ищем R.py
@@ -0,0 +1,9 @@
+a = []
+for n in range(1, 1000):
+    n2 = bin(n)[2:]
+    n2 += str(n2.count('1') % 2)
+    n2 += str(n2.count('1') % 2)
+    r = int(n2, 2)
+    if r > 720:
+        a.append(r)
+print(min(a))
